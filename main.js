953895a80067d93cea31829aec76b88c05f6af11
jieunboy0516 <jieunboy0516@gmail.com>
2022-10-13 01:35:49 +0100

    as

--- main.js
+++ main.js
@@ -167,7 +167,7 @@ function generateReply(){
 //prevent idle
 var http = require("http");
 setInterval(function() {
-    http.get("https://iu-hate-ethos.herokuapp.com/");
+    http.get("http://iu-hate-ethos.herokuapp.com/");
 }, 300000); // every 5 minutes (300000)
 
 
